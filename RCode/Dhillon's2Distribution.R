3b21108b0f08c634f4262c90c1240661b146b412
JaeseongYoo <praster1@gmail.com>
2018-11-16 04:42:44 +0900

    Create Dhillon's2Distribution.R

---
+++ RCode/Dhillon's2Distribution.R
@@ -0,0 +1,139 @@
+setwd("/home/lv999/Dropbox/Github/SurvivalAnalysis/RCode")
+source("colorPalette.R")
+
+
+##### dhillon2 Distribution
+### parameter
+alpha = c(-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1)
+beta = c(0, 0.25, 0.5, 0.75, 1, 2, 4, 8)
+gamma = c(0, 0.25, 0.5, 0.75, 1, 2, 4, 8)
+
+### input varialbe
+x <- seq(0, 1, length.out = 1000)
+
+
+### 수명 분포
+ddhillon2 <- function(x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx = ((gamma + 1) / (x - alpha + beta)) * (log(((x - alpha) / beta) + 1))^gamma * exp(-(log(((x - alpha) / beta) + 1))^(gamma+1))
+    return(fx)
+}
+
+
+### 누적분포함수
+pdhillon2 = function(x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx = -(sdhillon2(x, alpha, beta) - 1)
+    return(fx)
+}
+
+
+### 생존함수
+sdhillon2 = function (x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx <- ((gamma + 1) / (x - alpha + beta)) * (log(((x - alpha) / beta) + 1))^gamma
+    return(fx)
+}
+
+
+### 위험함수
+hdhillon2 = function (x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx <- ddhillon2(x, alpha, beta, gamma) / sdhillon2(x, alpha, beta, gamma)
+    return(fx)
+}
+
+
+
+
+
+##### Plot
+plot.dhillon2_seq = function(x, alpha = 1, beta = 1, gamma = 1, xlim=c(0, 10), ylim=c(0, 5), func="ddhillon2")
+{
+    color=colorPalette(300)
+
+    len_alpha = length(alpha)       # alpha 파라메터의 길이
+    len_beta = length(beta)          # beta 파라메터의 길이
+    len_gamma = length(gamma)  # gamma 파라메터의 길이
+    
+    color_counter = 1
+    for (i in 1:len_alpha)  ### 파라메터: alpha
+    {
+        if (func=="ddhillon2")     # 수명분포
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, ddhillon2(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Life Distribution Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, ddhillon2(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", i, " / beta = ", j, " / gamma = ", k, sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+        else if (func == "pdhillon2")  # 누적분포함수
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, pdhillon2(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Cualphalative Distribution Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, pdhillon2(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", i, " / beta = ", j, " / gamma = ", k, sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+        else if (func == "sdhillon2")  # 생존함수
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, sdhillon2(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Survival Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, sdhillon2(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", i, " / beta = ", j, " / gamma = ", k, sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+        else if (func == "hdhillon2")  # 위험함수
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, hdhillon2(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Hazard Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, hdhillon2(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", i, " / beta = ", j, " / gamma = ", k, sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+    }
+}
+
+par(mfrow = c(9, 8))
+plot.dhillon2_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-10, 10), func="ddhillon2")
+
+par(mfrow = c(9, 8))
+plot.dhillon2_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-5, 5), func="pdhillon2")
+
+par(mfrow = c(9, 8))
+plot.dhillon2_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-5, 5), func="sdhillon2")
+
+par(mfrow = c(9, 8))
+plot.dhillon2_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-30, 30), func="hdhillon2")
