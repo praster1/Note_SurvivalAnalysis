5fc3bd7998469cde5c407354f3c63fb37af93b87
JaeseongYoo <praster1@gmail.com>
2018-11-17 05:41:52 +0900

    Update RCode/generalizedLomaxDistribution.R

---
+++ RCode/generalizedLomaxDistribution.R
@@ -0,0 +1,141 @@
+# https://www.rdocumentation.org/packages/VGAM/versions/0.7-7/topics/glomax
+
+setwd("/home/lv999/Dropbox/Github/SurvivalAnalysis/RCode")
+source("colorPalette.R")
+
+
+##### glomax Distribution
+### parameter
+alpha = c(-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1)
+beta = c(0.25, 0.5, 0.75, 1, 2, 4, 8)
+gamma = c(-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1)
+
+### input varialbe
+x <- seq(0, 1, length.out = 1000)
+
+
+### 수명 분포
+dglomax <- function(x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx = (1/beta) * (1 + gamma * ((x - alpha)/beta))^(-(1 + 1/gamma))
+    return(fx)
+}
+
+
+### 누적분포함수
+pglomax = function(x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx = -(sglomax(x, alpha, beta) - 1)
+    return(fx)
+}
+
+
+### 생존함수
+sglomax = function (x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx <- (1/beta) * (1 + gamma * ((x - alpha)/beta))^(-1)
+    return(fx)
+}
+
+
+### 위험함수
+hglomax = function (x, alpha = 1, beta = 1, gamma = 1)
+{
+    fx <- dglomax(x, alpha, beta, gamma) / sglomax(x, alpha, beta, gamma)
+    return(fx)
+}
+
+
+
+
+
+##### Plot
+plot.glomax_seq = function(x, alpha = 1, beta = 1, gamma = 1, xlim=c(0, 10), ylim=c(0, 5), func="dglomax")
+{
+    color=colorPalette(300)
+
+    len_alpha = length(alpha)       # alpha 파라메터의 길이
+    len_beta = length(beta)          # beta 파라메터의 길이
+    len_gamma = length(gamma)  # gamma 파라메터의 길이
+    
+    color_counter = 1
+    for (i in 1:len_alpha)  ### 파라메터: alpha
+    {
+        if (func=="dglomax")     # 수명분포
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, dglomax(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Life Distribution Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, dglomax(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", alpha[i], " / beta = ", beta[j], " / gamma = ", gamma[k], sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+        else if (func == "pglomax")  # 누적분포함수
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, pglomax(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Cumulative Distribution Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, pglomax(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", alpha[i], " / beta = ", beta[j], " / gamma = ", gamma[k], sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+        else if (func == "sglomax")  # 생존함수
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, sglomax(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Survival Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, sglomax(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", alpha[i], " / beta = ", beta[j], " / gamma = ", gamma[k], sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+        else if (func == "hglomax")  # 위험함수
+        {
+            for (j in 1:len_beta)   ### 파라메터: beta
+            {
+                color_counter_init = color_counter
+                legend_name = NULL;
+                plot(x, hglomax(x, alpha=alpha[1], beta=beta[1], gamma=gamma[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Hazard Function")
+                for (k in 1:len_gamma)   ### 파라메터: gamma
+                {
+                    lines(x, hglomax(x, alpha=alpha[i], beta=beta[j], gamma=gamma[k]), col=color[color_counter], lwd=2);
+                    color_counter = color_counter + 1;
+                    legend_name = c(legend_name, paste("alpha = ", alpha[i], " / beta = ", beta[j], " / gamma = ", gamma[k], sep=""))
+                }
+                legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+            }
+        }
+    }
+}
+
+par(mfrow = c(8, 8))
+plot.glomax_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-10, 10), func="dglomax")
+
+par(mfrow = c(8, 8))
+plot.glomax_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-5, 5), func="pglomax")
+
+par(mfrow = c(8, 8))
+plot.glomax_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-5, 5), func="sglomax")
+
+par(mfrow = c(8, 8))
+plot.glomax_seq(x, alpha, beta, gamma, xlim=c(min(x), max(x)), ylim=c(-30, 30), func="hglomax")
