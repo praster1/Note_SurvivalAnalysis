17e4bfb9f9b24fa17bab95978f14ccd74e4c0430
JaeseongYoo <praster1@gmail.com>
2018-11-17 04:20:14 +0900

    Update RCode/linearHazardRateDistribution.R

--- RCode/linearHazardRateDistribution.R
+++ RCode/linearHazardRateDistribution.R
@@ -1,95 +1,118 @@
+setwd("/home/lv999/Dropbox/Github/SurvivalAnalysis/RCode")
+source("colorPalette.R")
+
+
 ##### 선형 증가 분포
-dlinearFR = function (x, alpha = 0, beta = 1, log = FALSE) 
+### parameter
+alpha = c(0, 0.25, 0.5, 0.75, 1, 2, 4, 8)	# m (delta)
+beta = c(0.25, 0.5, 0.75, 1, 2, 4, 8)		# s (beta)
+
+### input varialbe
+x <- seq(0, 10, length.out = 1000)
+
+
+### 수명 분포
+dlinearFR = function(x, alpha = 0, beta = 1)
 {
     fx <- (alpha * x + beta) * exp(- (1/2) * alpha * x^2 + beta * x)
-    if (log) 
-        return(log(fx))
-    else return(fx)
+    return(fx)
 }
 
-plinearFR = function (q, alpha = 0, beta = 1, lower.tail = TRUE, log.p = FALSE) 
+
+### 누적분포함수
+plinearFR = function(x, alpha = 0, beta = 1)
 {
     Fx <- 1 - exp(- (1/2) * alpha * x^2 + beta * x)
-    if (!lower.tail) 
-        Fx <- 1 - Fx
-    if (log.p) 
-        Fx <- log(Fx)
     return(Fx)
 }
 
-# 점검 필요
-# qlinearFR = function (p, scale = 1, location = 0, lower.tail = TRUE, log.p = FALSE) 
-# {
-    # if (log.p) 
-        # p <- exp(p)
-    # if (!lower.tail) 
-        # p <- 1 - p
-    # xF <- location - scale * log(-log(p))
-    # return(xF)
-# }
 
-# rlinearFR = function (n, alpha = 0, beta = 1) 
-# {
-	# qlinearFR(runif(n), scale, location)
-# }
+### 생존함수
+slinearFR = function(x, alpha = 0, beta = 1)
+{
+    fx <- 1 - plinearFR(x, alpha = alpha, beta = beta)
+    return(fx)
+}
 
 
-par(mfrow = c(2, 4))
-
-### parameter: alpha
-alpha = c(0.5, 1, 2, 4, 8)	# shape
-
-### Input Variable
-x <- seq(0, 10, length.out = 101)
-
-color = rainbow(10)
-
-### Life Distribution
-plot(x, dlinearFR(x, alpha=alpha[1], beta=1), xlim=c(0, 10), ylim=c(0, 6), col=color[1], lwd=2, type = 'l', main="Life Distribution")
-for (i in 2:5)	{	lines(x, dlinearFR(x, alpha=alpha[i], beta=1), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('alpha = 0.5', 'alpha = 1', 'alpha = 2', 'alpha = 4', 'alpha = 8'))
-
-### Cumulative Distribution
-plot(x, plinearFR(x, alpha=alpha[1], beta=1), xlim=c(0, 10), ylim=c(0, 1), col=color[1], lwd=2, type = 'l', main="Cumulative Distribution")
-for (i in 2:5)	{	lines(x, plinearFR(x, alpha=alpha[i], beta=1), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('alpha = 0.5', 'alpha = 1', 'alpha = 2', 'alpha = 4', 'alpha = 8'))
-
-### Survival Function
-plot(x, 1-plinearFR(x, alpha=alpha[1], beta=1), xlim=c(0, 10), ylim=c(0, 3), col=color[1], lwd=2, type = 'l', main="Survival Function")
-for (i in 2:5)	{	lines(x, 1-plinearFR(x, alpha=alpha[i], beta=1), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('alpha = 0.5', 'alpha = 1', 'alpha = 2', 'alpha = 4', 'alpha = 8'))
-
-### Hazard Function
-plot(x, dlinearFR(x, alpha=alpha[1], beta=1)/(1-plinearFR(x, alpha=alpha[1], beta=1)), xlim=c(0, 10), ylim=c(0, 10), col=color[1], lwd=2, type = 'l', main="Hazard Function")
-for (i in 2:5) {	lines(x, dlinearFR(x, alpha=alpha[i], beta=1)/(1-plinearFR(x, alpha=alpha[i], beta=1)), col=color[i], lwd=2);	}
-legend('topright', bty = 'n', lwd=2, col=color[1:5], legend = c('alpha = 0.5', 'alpha = 1', 'alpha = 2', 'alpha = 4', 'alpha = 8'))
+### 위험함수
+hlinearFR = function(x, alpha = 0, beta = 1)
+{
+    fx <- dlinearFR(x, alpha = alpha, beta = beta) / slinearFR(x, alpha = alpha, beta = beta)
+    return(fx)
+}
 
 
 
-### parameter: beta_p
-beta_p = c(0.5, 1, 2, 4, 8)	#beta
 
-### Input Variable
-x <- seq(-10, 10, length.out = 101)
 
-color = rainbow(10)
+##### Plot
+plot.linearFR_seq = function(x, alpha = 0, beta = 1, xlim=c(0, 10), ylim=c(0, 5), func="dlinearFR")
+{
+    color=colorPalette(300)
 
-### Life Distribution
-plot(x, dlinearFR(x, alpha=0, beta=beta_p[1]), xlim=c(-5, 10), ylim=c(0, 6), col=color[1], lwd=2, type = 'l', main="Life Distribution")
-for (i in 2:5)	{	lines(x, dlinearFR(x, alpha=0, beta=beta_p[i]), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('beta_p = 0.5', 'beta_p = 1', 'beta_p = 2', 'beta_p = 4', 'beta_p = 8'))
+    len_alpha = length(alpha)       # m 파라메터의 길이
+    len_beta = length(beta)          # s 파라메터의 길이
+    
+    color_counter = 1
+    for (i in 1:len_alpha)  ### 파라메터: m
+    {
+        color_counter_init = color_counter
+        legend_name = NULL;
+        
+        if (func=="dlinearFR")     # 수명분포
+        {
+            plot(x, dlinearFR(x, alpha=alpha[1], beta=beta[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Life Distribution Function")
+            for (j in 1:len_beta)   ### 파라메터: s
+            {
+                lines(x, dlinearFR(x, alpha=alpha[i], beta=beta[j]), col=color[color_counter], lwd=2);
+                color_counter = color_counter + 1;
+                legend_name = c(legend_name, paste("alpha = ", m[i], " / beta = ", s[j], sep=""))
+            }
+        }
+        else if (func == "plinearFR")  # 누적분포함수
+        {
+            plot(x, plinearFR(x, alpha=alpha[1], beta=beta[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Cumulative Distribution Function")
+            for (j in 1:len_beta)   ### 파라메터: s
+            {
+                lines(x, plinearFR(x, alpha=alpha[i], beta=beta[j]), col=color[color_counter], lwd=2);
+                color_counter = color_counter + 1;
+                legend_name = c(legend_name, paste("alpha = ", m[i], " / beta = ", s[j], sep=""))
+            }
+        }
+        else if (func == "slinearFR")  # 생존함수
+        {
+            plot(x, slinearFR(x, alpha=alpha[1], beta=beta[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Survival Function")
+            for (j in 1:len_beta)   ### 파라메터: s
+            {
+                lines(x, slinearFR(x, alpha=alpha[i], beta=beta[j]), col=color[color_counter], lwd=2);
+                color_counter = color_counter + 1;
+                legend_name = c(legend_name, paste("alpha = ", m[i], " / beta = ", s[j], sep=""))
+            }
+        }
+        else if (func == "hlinearFR")  # 위험함수
+        {
+            plot(x, hlinearFR(x, alpha=alpha[1], beta=beta[1]), xlim=xlim, ylim=ylim, col=color[1], lwd=2, type = 'n', main="Hazard Function")
+            for (j in 1:len_beta)   ### 파라메터: s
+            {
+                lines(x, hlinearFR(x, alpha=alpha[i], beta=beta[j]), col=color[color_counter], lwd=2);
+                color_counter = color_counter + 1;
+                legend_name = c(legend_name, paste("alpha = ", m[i], " / beta = ", s[j], sep=""))
+            }
+        }
+        legend('right', bty = 'n', lwd=2, col=color[color_counter_init:(color_counter - 1)], legend = legend_name)
+    }
+}
 
-### Cumulative Distribution
-plot(x, plinearFR(x, alpha=0, beta=beta_p[1]), xlim=c(-5, 10), ylim=c(0, 1), col=color[1], lwd=2, type = 'l', main="Cumulative Distribution")
-for (i in 2:5)	{	lines(x, plinearFR(x, alpha=0, beta=beta_p[i]), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('beta_p = 0.5', 'beta_p = 1', 'beta_p = 2', 'beta_p = 4', 'beta_p = 8'))
+par(mfrow = c(3, 3))
+plot.linearFR_seq(x, alpha, beta, xlim=c(min(x), max(x)), ylim=c(0, 50), func="dlinearFR")
 
-### Survival Function
-plot(x, 1-plinearFR(x, alpha=0, beta=beta_p[1]), xlim=c(-5, 10), ylim=c(0, 3), col=color[1], lwd=2, type = 'l', main="Survival Function")
-for (i in 2:5)	{	lines(x, 1-plinearFR(x, alpha=0, beta=beta_p[i]), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('beta_p = 0.5', 'beta_p = 1', 'beta_p = 2', 'beta_p = 4', 'beta_p = 8'))
+par(mfrow = c(3, 3))
+plot.linearFR_seq(x, alpha, beta, xlim=c(min(x), max(x)), ylim=c(0, 1), func="plinearFR")
 
-### Hazard Function
-plot(x, dlinearFR(x, alpha=0, beta=beta_p[1])/(1-plinearFR(x, alpha=0, beta=beta_p[1])), xlim=c(-5, 10), ylim=c(0, 10), col=color[1], lwd=2, type = 'l', main="Hazard Function")
-for (i in 2:5) {	lines(x, dlinearFR(x, alpha=0, beta=beta_p[i])/(1-plinearFR(x, alpha=0, beta=beta_p[i])), col=color[i], lwd=2);	}
-legend('right', bty = 'n', lwd=2, col=color[1:5], legend = c('beta_p = 0.5', 'beta_p = 1', 'beta_p = 2', 'beta_p = 4', 'beta_p = 8'))
+par(mfrow = c(3, 3))
+plot.linearFR_seq(x, alpha, beta, xlim=c(min(x), max(x)), ylim=c(0, 1), func="slinearFR")
+
+par(mfrow = c(3, 3))
+plot.linearFR_seq(x, alpha, beta, xlim=c(min(x), max(x)), ylim=c(0, 50), func="hlinearFR")
+
