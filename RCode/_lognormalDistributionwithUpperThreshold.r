75c08a827e830a6b40e5aa0d827d7653447d09af
JaeseongYoo <praster1@gmail.com>
2018-11-18 00:12:29 +0900

    Log Normal Distribution with Threshold

---
+++ RCode/_lognormalDistributionwithUpperThreshold.r
@@ -0,0 +1,6 @@
+requireFAdist
+ 
+dlnorm3(x,shape=1,scale=1,thres=0,log=FALSE)
+plnorm3(q,shape=1,scale=1,thres=0,lower.tail=TRUE,log.p=FALSE)
+qlnorm3(p,shape=1,scale=1,thres=0,lower.tail=TRUE,log.p=FALSE)
+rlnorm3(n,shape=1,scale=1,thres=0)
