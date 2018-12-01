13ecee5abbc797885558d8925aa18457e88b5fa9
JaeseongYoo <praster1@gmail.com>
2018-12-02 06:44:26 +0900

    Update exponentialDistributionwithLocationParameter.R

--- Distributions/exponentialDistributionwithLocationParameter.R
+++ Distributions/exponentialDistributionwithLocationParameter.R
@@ -20,6 +20,17 @@ dlexponential = function(x, alpha=alpha, beta=beta)
 }
 
 
+### 난수 함수
+rlexponential = function(n, min=0.1, max=1, alpha = 1, beta = 1)
+{
+	normalization = function(x)	{	(x-min(x))/(max(x)-min(x));	}
+
+	xseq = seq(min, max, length=1000000)
+	res = sample(xseq, size=n, prob=normalization(dlexponential(xseq, alpha=alpha, beta=beta)), replace=TRUE)
+	return(res)
+}
+
+
 ### 누적분포함수
 plexponential = function(x, alpha=alpha, beta=beta)
 {
