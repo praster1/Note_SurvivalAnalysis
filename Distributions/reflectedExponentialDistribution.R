a7ea394deb70f2ea64d85113b1e31afa1c265fd0
JaeseongYoo <praster1@gmail.com>
2018-12-02 13:54:06 +0900

    Update reflectedExponentialDistribution.R

--- Distributions/reflectedExponentialDistribution.R
+++ Distributions/reflectedExponentialDistribution.R
@@ -12,15 +12,26 @@ x = seq(0.1, 10, length.out = 1000)
 
 
 ### 수명 분포
-drexponential = function(x, alpha=alpha, beta=beta)
+drexponential = function(x, alpha=1, beta=1)
 {
     fx = (1/beta) * exp((x-alpha)/beta)
     return(fx)
 }
 
 
+### 난수 함수
+rrexponential = function (n, min=0.0001, max=10, alpha = 1, beta = 1)
+{
+    normalization = function(x)	{	(x-min(x))/(max(x)-min(x));	}
+
+	xseq = seq(min, max, length=1000000)
+	res = sample(xseq, size=n, prob=normalization(drexponential(xseq, alpha = alpha, beta = beta)), replace=TRUE)
+	return(res)
+}
+
+
 ### 누적분포함수
-prexponential = function(x, alpha=alpha, beta=beta)
+prexponential = function(x, alpha=1, beta=1)
 {
     fx = -(srexponential(x, alpha, beta) - 1)
     return(fx)
@@ -28,7 +39,7 @@ prexponential = function(x, alpha=alpha, beta=beta)
 
 
 ### 생존함수
-srexponential = function (x, alpha=alpha, beta=beta)
+srexponential = function (x, alpha=1, beta=1)
 {
     fx = 1 - exp((x-alpha)/beta)
     return(fx)
@@ -36,7 +47,7 @@ srexponential = function (x, alpha=alpha, beta=beta)
 
 
 ### 위험함수
-hrexponential = function (x, alpha=alpha, beta=beta)
+hrexponential = function (x, alpha=1, beta=1)
 {
     fx = drexponential(x, alpha, beta) / srexponential(x, alpha, beta)
     return(fx)
