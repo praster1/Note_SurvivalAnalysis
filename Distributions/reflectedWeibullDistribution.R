f61233e4bf8a4a666d59feffe2969257c51a25db
JaeseongYoo <praster1@gmail.com>
2018-12-02 14:38:54 +0900

    Update reflectedWeibullDistribution.R

--- Distributions/reflectedWeibullDistribution.R
+++ Distributions/reflectedWeibullDistribution.R
@@ -21,6 +21,17 @@ dreflectedweibull = function(x, alpha=0, beta=1, gamma=1)
 }
 
 
+### 난수 함수
+rreflectedweibull = function (n, min=-10, max=10, alpha = 0, beta = 1, gamma = 1)
+{
+    normalization = function(x)	{	(x-min(x))/(max(x)-min(x));	}
+
+	xseq = seq(min, max, length=1000000)
+	res = sample(xseq, size=n, prob=normalization(dreflectedweibull(xseq, alpha = alpha, beta = beta, gamma = gamma)), replace=TRUE)
+	return(res)
+}
+
+
 ### 누적분포함수
 preflectedweibull = function(x, alpha=0, beta=1, gamma=1)
 {
