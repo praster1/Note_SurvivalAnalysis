cf52db382e71e7dd16054410529aa0b2f738d04a
JaeseongYoo <praster1@gmail.com>
2018-12-02 13:46:44 +0900

    Update paretoDistributionoftheFirstKind.R

--- Distributions/paretoDistributionoftheFirstKind.R
+++ Distributions/paretoDistributionoftheFirstKind.R
@@ -20,6 +20,17 @@ dpareto1 = function(x, alpha = 1, beta = 1, gamma = 1)
 }
 
 
+### 난수 함수
+rpareto1 = function (n, min=0.0001, max=10, alpha = 1, beta = 2, gamma = 1) 
+{
+    normalization = function(x)	{	(x-min(x))/(max(x)-min(x));	}
+
+	xseq = seq(min, max, length=1000000)
+	res = sample(xseq, size=n, prob=normalization(dpareto1(xseq, alpha = alpha, beta = beta, gamma = gamma)), replace=TRUE)
+	return(res)
+}
+
+
 ### 누적분포함수
 ppareto1 = function(x, alpha = 1, beta = 1, gamma = 1)
 {
