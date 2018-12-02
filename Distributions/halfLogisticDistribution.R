3055ab44928e9efc0f9e85c39482a620622e64ad
JaeseongYoo <praster1@gmail.com>
2018-12-02 11:32:57 +0900

    Update halfLogisticDistribution.R

--- Distributions/halfLogisticDistribution.R
+++ Distributions/halfLogisticDistribution.R
@@ -19,6 +19,16 @@ dhalflogistic = function(x, alpha = 0, beta = 1)
 }
 
 
+### 난수 함수
+rhalflogistic = function (n, min=0.0001, max=1, alpha = 1, beta = 1) 
+{
+    normalization = function(x)	{	(x-min(x))/(max(x)-min(x));	}
+
+	xseq = seq(min, max, length=1000000)
+	res = sample(xseq, size=n, prob=normalization(dhalflogistic(xseq, alpha = alpha, beta = beta)), replace=TRUE)
+	return(res)
+}
+
 ### 누적분포함수
 phalflogistic = function(x, alpha = 0, beta = 1)
 {
