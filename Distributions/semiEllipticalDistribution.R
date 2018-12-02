c76f4e5b16c6ba92ed54432d9d15eaaf760660ec
JaeseongYoo <praster1@gmail.com>
2018-12-03 02:14:46 +0900

    Update semiEllipticalDistribution.R

--- Distributions/semiEllipticalDistribution.R
+++ Distributions/semiEllipticalDistribution.R
@@ -9,7 +9,7 @@ alpha = c(-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1)
 beta = c(0.25, 0.5, 0.75, 1, 2, 4, 8)
 
 ### input varialbe
-x = seq(-10, 10, length.out = 1000)
+x = seq(-1, 1, length.out = 1000)
 
 
 ### 수명 분포
@@ -18,6 +18,18 @@ dsemielliptical = function(x, alpha = 0, beta = 1)
     fx = (2/(beta * pi)) * sqrt(1 - ((x-alpha)/beta)^2)
     return(fx)
 }
+
+
+### 난수 함수
+rsemielliptical = function (n, min=-1, max=1, alpha = 0, beta = 1)
+{
+    normalization = function(x)	{	(x-min(x))/(max(x)-min(x));	}
+
+	xseq = seq(min, max, length=1000000)
+	res = sample(xseq, size=n, prob=normalization(dsemielliptical(xseq, alpha = alpha, beta = beta)), replace=TRUE)
+	return(res)
+}
+
 
 
 ### 누적분포함수
